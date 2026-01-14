1d4fb8a1944013c5dade45fac11771688132abb8
Arthur Shaw <jashaw2002@gmail.com>
2026-01-13 19:24:44 -0500

    feat(file paths): add function that returns list of all paths

---
+++ R/construct_paths.R
@@ -0,0 +1,97 @@
+#' Construct paths for the project
+#'
+#' @return List. Collection of all project paths, available as named elements:
+#'
+#' ```yaml
+#' proj: <character> # project root
+#' r: <character> # R function directory
+#' data: <character>
+#'  meta: <character>
+#'  demand:
+#'    download: <character>
+#'    combine: <character>
+#'  community:
+#'    download: <character>
+#'    combine: <character>
+#'  supply:
+#'    download: <character>
+#'    combine: <character>
+#' validation:
+#'  demand: <character>
+#'  community: <character>
+#'  supply: <character>
+#' ```
+#'
+#' @importFrom here here
+#' @importFrom fs path
+#' @importFrom purrr map
+construct_paths <- function() {
+
+  # ============================================================================
+  # top-level
+  # ============================================================================
+
+  dirs <- list()
+  dirs$proj <- here::here()
+  dirs$r <- here::here("R")
+  dir_data <- here::here("01_data")
+  dir_validation <- here::here("01_validation")
+
+
+  # ============================================================================
+  # data
+  # ============================================================================
+
+  # ----------------------------------------------------------------------------
+  # add an entry for the metadata
+  # ----------------------------------------------------------------------------
+
+  dirs$data <- list(
+    meta = fs::path(dir_data, "00_meta")
+  )
+
+  # ----------------------------------------------------------------------------
+  # add sub-directory entries for all surveys
+  # ----------------------------------------------------------------------------
+
+  # use a named vector so that the list entries can be named
+  # with the values being the directory name
+  data_parents <- c(
+    demand    = "01_demand",
+    community = "02_community",
+    supply    = "03_supply"
+  )
+
+  # apply a function to each a list of sub-directory paths
+  # to each list entry named after a survey
+  dirs$data[names(data_parents)] <- data_parents |>
+    purrr::map(
+      .f = \(parent_dir_name) {
+
+        # construct the full path to the parent directory
+        parent_dir <- fs::path(dir_data, parent_dir_name)
+
+        # construct sub-directories under the parent
+        child_dirs <- list(
+          download = fs::path(parent_dir, "01_downloaded"),
+          combine = fs::path(parent_dir, "02_combined")
+        )
+
+        return(child_dirs)
+
+      }
+    )
+
+  # ============================================================================
+  # validation
+  # ============================================================================
+
+  dirs$validation <- list(
+    demand = fs::path(dir_validation, "01_demand"),
+    community = fs::path(dir_validation, "02_community"),
+    supply = fs::path(dir_validation, "03_supply")
+  )
+
+  return(dirs)
+
+}
