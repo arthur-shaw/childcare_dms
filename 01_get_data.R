8b97d49e51fbac16c01a79c683ebae3cc16bb583
Arthur Shaw <jashaw2002@gmail.com>
2026-03-16 18:02:59 -0400

    feat(get data): add script to orchestrate

---
+++ 01_get_data.R
@@ -0,0 +1,45 @@
+# ==============================================================================
+# get data 💾
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# microdata
+# ------------------------------------------------------------------------------
+
+qnr_types <- c("demand", "supply")
+
+dl_specs <- tibble::tribble(
+  ~ qnr_expr, ~ type,
+  qnr_demand, "demand",
+  qnr_supply, "supply",
+)
+
+purrr::pwalk(
+  .l = dl_specs,
+  .f = ~ get_data(
+    qnr_expr = ..1,
+    allowed_types = qnr_types,
+    type = ..2,
+    dirs = dirs,
+    server = server,
+    workspace = workspace,
+    user = user,
+    password = password,
+    get_msg = get_msg
+  )
+
+)
+
+# ------------------------------------------------------------------------------
+# team composition
+# ------------------------------------------------------------------------------
+
+cli::cli_alert_info(get_msg("get_data", "getting_metadata"))
+
+get_team_composition(
+  dir = dirs$data$meta,
+  server = server,
+  workspace = workspace,
+  user = user,
+  password = password
+)
